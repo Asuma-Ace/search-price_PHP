2f321b409f92bb84c0d84d89413ba612e9b3ec79
samua <asuma.business2@gmail.com>
2020-09-04 22:50:38 +0900

    add procfile

--- dbconnect.php
+++ dbconnect.php
@@ -1,10 +1,10 @@
 <?php
 function dbConnect(){
   $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
-  $db['dbname'] = ltrim($db['path'], '/');
-  $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
-  $user = $db['user'];
-  $password = $db['pass'];
+  $db['heroku_30ebda75726157d'] = ltrim($db['path'], '/');
+  $dsn = "mysql:host={$db['us-cdbr-east-02.cleardb.com']};dbname={$db['heroku_30ebda75726157d']};port=3306;charset=utf8";
+  $user = $db['b080b700f88850'];
+  $password = $db['ad84688d'];
   $options = array(
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
