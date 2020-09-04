4f55ba40e1916c2bd1f537647a2c99317acb2bbe
samua <asuma.business2@gmail.com>
2020-09-04 17:35:20 +0900

    change dbname

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,7 @@
 <?php
 try {
   $url = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
-  $db_name = ltrim($url['path'], '/');
+  $db_name = 'search-price-db';
   $db_host = 'us-cdbr-east-02.cleardb.com';
   $user = 'b080b700f88850';
   $password = 'ad84688d';
