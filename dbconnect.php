d16b3abb5faabf6f49c790a3759a1d5ed43f3697
samua <asuma.business2@gmail.com>
2020-09-05 08:08:33 +0900

    modify dbconnect

--- dbconnect.php
+++ dbconnect.php
@@ -1,17 +1,18 @@
 <?php
-function dbConnect(){
-  $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
-  $db['dbname'] = ltrim($db['path'], '/');
-  $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
-  $user = $db['user'];
-  $password = $db['pass'];
-  $options = array(
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
+try {
+  $db = new PDO(
+    'mysql:dbname=heroku_30ebda75726157d;host=us-cdbr-east-02.cleardb.com;charset=utf8',
+    'b080b700f88850',
+    'ad84688d',
+    [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+      PDO::ATTR_EMULATE_PREPARES => false,
+    ]
   );
-  $dbh = new PDO($dsn,$user,$password,$options);
-  return $dbh;
-  }
-  $dbh = dbConnect();
+} catch(PDOException $e) {
+    // header('Content-Type: text/plain; charset=UTF-8', true, 500);
+    exit('データベース接続エラー: '. $e->getMessage());
+}
+header('Content-Type: text/html; charset=utf-8');
 ?>
