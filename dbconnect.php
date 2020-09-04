3d8f5ef1b756abec3e04997fb07c4864ebcc8da7
samua <asuma.business2@gmail.com>
2020-09-05 07:55:26 +0900

    modify dbconnect

--- dbconnect.php
+++ dbconnect.php
@@ -13,8 +13,5 @@ function dbConnect(){
   $dbh = new PDO($dsn,$user,$password,$options);
   return $dbh;
   }
-    ini_set('display_errors', 1);
-ini_set('error_reporting', E_ALL);
   $dbh = dbConnect();
-
 ?>
